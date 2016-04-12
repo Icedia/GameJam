2c2f1fd9d8e41d6907fc9b4ede220bec3e54ae1e
Icedia <daniel1995@live.nl>
2016-04-12 10:10:51 +0200

    pushing again

---
+++ GameJam/Assets/Scripts/OffsetScroller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+
+    void Update()
+    {
+        float y = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        Vector2 offset = new Vector2(0, y);
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
---
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float _speed;
+    public float _fallingSpeed;
+
+	void Update () 
+	{
+        transform.position += new Vector3(0, -_fallingSpeed);
+
+
+		if (Input.GetKey (KeyCode.LeftArrow))
+		{
+			transform.position -= new Vector3 (_speed * Time.deltaTime, 0.0f, 0f);
+
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.position += new Vector3 (_speed * Time.deltaTime, 0.0f, 0f);
+		}
+
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.transform.tag == "GoodBug")
+        {
+            _fallingSpeed = _fallingSpeed + 0.5f;
+            Destroy(other.gameObject);
+
+        }
+        if (other.transform.tag == "BadBug")
+        {
+            _fallingSpeed = _fallingSpeed - 0.5f;
+            Destroy(other.gameObject);
+
+        }
+    }
+	
+}
